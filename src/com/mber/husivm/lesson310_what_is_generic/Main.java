757d222e04dce457114444dc6d8013002a615670
M10beretta <Mberetta.gm@gmail.com>
2022-01-06 11:26:02 +0300

    commit

---
+++ src/com/mber/husivm/lesson310_what_is_generic/Main.java
@@ -0,0 +1,42 @@
+package com.mber.husivm.lesson310_what_is_generic;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Gen<String> stringGen = new Gen<>();
+        stringGen.setT("Hi");
+        String stringGenT = stringGen.getT();
+        System.out.println(stringGenT);
+
+        GenObj stringGenObj = new GenObj();
+        stringGenObj.setT("Hi");
+        Object stringGenObjT = stringGenObj.getT();
+        System.out.println(stringGenObjT);
+
+    }
+}
+
+class Gen<T>{
+    private T t;
+
+    public T getT() {
+        return t;
+    }
+
+    public void setT(T t) {
+        this.t = t;
+    }
+}
+
+class GenObj{
+    private Object t;
+
+    public Object getT() {
+        return t;
+    }
+
+    public void setT(Object t) {
+        this.t = t;
+    }
+}
